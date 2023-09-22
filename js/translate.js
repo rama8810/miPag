456c7adcb917beb680bcdee405ae6cc4b0ab0945
rama8810 <ing.rama88@gmail.com>
2023-09-22 13:53:31 -0500

    delete google translate

--- js/translate.js
+++
@@ -1,9 +0,0 @@
-
-function googleTranslateElementInit() {
-	new google.translate.TranslateElement({
-        pageLanguage: 'es', includedLanguages: 'en,fr', 
-        layout: google.translate.TranslateElement.InlineLayout.SIMPLE, 
-        gaTrack: true}, 'google_translate_element');
-}
-
-
